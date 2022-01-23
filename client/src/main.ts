2b5aad59c90c940d52fee723de8a91c2b1271964
NilsRamstoeck <nilsramstoeck@gmail.com>
2022-01-23 15:19:11 +0000

    - set min size for window & added menu for debugging

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,19 +1,45 @@
 import {
    app,
    BrowserWindow,
+   Menu,
+   MenuItem,
    screen,
 } from 'electron';
 
 app.whenReady().then(() => {
    const { width, height } = screen.getPrimaryDisplay().workAreaSize
    const win = new BrowserWindow({
-      width: width,
-      height: height,
-      x: 0,
-      y: 0,
+      width: width/2,
+      height: height/2,
+      minWidth: width/2,
+      minHeight: height/2,
+      center: true,
       webPreferences: {
          nodeIntegration: true,
       }
    });
    win.loadFile('./index.html');
+
+   const menu = new Menu();
+   menu.append(new MenuItem({
+      label: 'File',
+      submenu: [
+         {
+            label: 'Toggle Developer Tools',
+            accelerator: 'ctrl+shift+i',
+            click: () => {BrowserWindow.getFocusedWindow().toggleDevTools()}
+         },         {
+            label: 'Reload',
+            accelerator: 'f5',
+            click: () => {BrowserWindow.getFocusedWindow().reload()}
+         },
+         {
+            label: 'Exit',
+            accelerator: 'esc',
+            click: () => { app.quit() }
+         },
+      ]
+   }));
+
+   Menu.setApplicationMenu(menu);
 })
