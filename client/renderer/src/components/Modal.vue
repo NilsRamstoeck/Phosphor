8a35498cf62a14722f15275152bb0c2504c09600
NilsRamstoeck <nilsramstoeck@gmail.com>
2022-01-30 16:06:49 +0100

    - styling adjustments

--- client/renderer/src/components/Modal.vue
+++ client/renderer/src/components/Modal.vue
@@ -11,28 +11,7 @@
 </template>
 
 <script>
-
+   export default {
+      name: 'modal'
+   }
 </script>
-
-<style scoped>
-   .modal{
-      position: fixed;
-   }
-   .modal-wrapper{
-      position: absolute;
-      height: 100vh;
-      width: 100vw;
-      background: rgba(0, 0, 0, 0.5)
-   }
-   .modal-content{
-      margin: auto auto;
-      height: max-content;
-      width: 30vw;
-      position: absolute;
-      left: 0;
-      right: 0;
-      top: 0;
-      bottom: 0;
-      background: #fff;
-   }
-</style>
